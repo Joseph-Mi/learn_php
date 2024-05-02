ddecc8c40d5dd660eb3c46d6e33bf2a68470148e
Joseph Mi <jmjosephmi@gmail.com>
2024-05-02 18:31:54 -0400

    calculator

---
+++ calculator.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <body>
+        <form action="calculator.php" method="get">
+            <input type="number" name="num1">
+            <br>
+            <input type="number" name="num2">
+            <input type="submit">
+
+        </form>
+
+        Answer: <?php echo $_GET["num1"] + $_GET["num2"] ?>
+    </body>
+</html>
--- site.php
+++ site.php
@@ -6,6 +6,19 @@
     </head>
     
     <body>
+
+        <form action="site.php" method = "get">
+            Name: <input type="text" name="username">
+            <br>
+            Age: <input type="text" name="age">
+            <br>
+            <input type="submit">
+        </form>
+        <br>
+
+        Your name is <?php echo $_GET["username"]; ?>
+        <br>
+        Your age is <?php echo $_GET["age"]; ?>
         
         <?php
             $charName = "Joseph";
@@ -36,6 +49,21 @@
             $phrase = substr($phrase, 0, 5);
 
             echo $phrase[0];
+
+            #####################NUMBERS#######################
+
+            echo "<p></p>"; #decimal number
+            echo (4-2.99) * 10;
+            echo abs(-31);
+            echo pow(10,0);
+            echo max(-10, -5);
+            echo round(3.2) #floor()= 3 brings down ciel() = 4 brings up
+
+            ####################USERINPUT#####################
+
+            #html form
+            
+
         ?>
     </body>
 </html>
