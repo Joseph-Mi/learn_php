6238e3d32d0318723a9194801779eee21d45235f
Joseph Mi <jmjosephmi@gmail.com>
2024-05-02 16:58:26 -0400

    numbers and strings

--- site.php
+++ site.php
@@ -8,7 +8,34 @@
     <body>
         
         <?php
-            echo("Hello World")
+            $charName = "Joseph";
+            $charAge = 19;
+
+            echo "Hello $charName";
+            echo "<hr>";
+
+            $charName = "Tom";
+
+            echo "<h1> $charName's Site </h1>";
+            echo "<p> I am $charAge!!! </p>";
+
+            #############################DATATYPES################################
+
+            $phrase = "To be or not to be"; #string
+            $age = 20; #integer
+            $gpa = 3.85; #float
+            $isHappy = true; #boolean
+
+            ####################STRINGS#######################
+
+            echo strtoupper("$charName dog\n");
+            $nameLength = strlen($charName);
+            echo str_replace("m","nia\n", $charName);
+            echo $charName;
+
+            $phrase = substr($phrase, 0, 5);
+
+            echo $phrase[0];
         ?>
     </body>
 </html>
