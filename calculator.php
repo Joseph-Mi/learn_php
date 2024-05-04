6cbe9838a03a092b62c108521a0f51398abdf08d
Joseph Mi <jmjosephmi@gmail.com>
2024-05-04 00:56:02 -0400

    post

--- calculator.php
+++ calculator.php
@@ -2,14 +2,20 @@
 
 <html>
     <body>
-        <form action="calculator.php" method="get">
+        <form action="calculator.php" method="post">
             <input type="number" name="num1">
             <br>
             <input type="number" name="num2">
+            Password: <input type="password" name="pw">
             <input type="submit">
 
         </form>
 
-        Answer: <?php echo $_GET["num1"] + $_GET["num2"] ?>
+        Answer: <?php echo $_POST["num1"] + $_POST["num2"] ?>
+        <?php
+            $sum = $_POST["num1"] + $_POST["num2"];
+            $pw = $_POST["pw"];
+            echo $pw;
+        ?>
     </body>
 </html>
